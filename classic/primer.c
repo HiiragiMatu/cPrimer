beec9c7ae744e03de1f8d211d01b6591a8930aae
Matsu <B10523058@gemail.yuntech.edu.tw>
2020-10-17 20:42:17 +0800

    Prime

---
+++ classic/primer.c
@@ -0,0 +1,29 @@
+/* Find Prime Ch7-17*/
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+  unsigned long num; // number to be checked
+  unsigned long div; // potnetial divisors
+  bool isPrime; // isPrimer is called 'flag'
+
+  printf("Please enter an integer for analysis(q to quit): ");
+  while(scanf("%lu", &num) == 1){
+    for (div = 2, isPrime = true; (div * div) <= num; div++){
+      if(num % div == 0)
+      {
+        if((div * div != num))
+          printf("%lu is divisible by %lu and %lu.\n", num, div, num / div);
+        else
+          printf("%lu is divisible by %lu.\n", num, div);
+
+        isPrime = false; // number is not a primer
+      }
+    }
+    if(isPrime)
+      printf("%lu is prime.\n", num);
+    printf("Enter the next number to checked(q to exit): ");
+  }5
+  printf("Bye!");
+  return 0;
+}
