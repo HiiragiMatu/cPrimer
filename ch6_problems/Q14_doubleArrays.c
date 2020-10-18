e76a5c1fad1fa738e07fc1717de89086f69c2fa8
Matsu <B10523058@gemail.yuntech.edu.tw>
2020-10-18 20:21:01 +0800

    Q14 fix

--- ch6_problems/Q14_doubleArrays.c
+++ ch6_problems/Q14_doubleArrays.c
@@ -4,17 +4,21 @@
 int main(void){
   int i, j;
   double nums[SIZE];
+  double map_nums[SIZE];
   double input;
   
   printf("Enter 8 doubles: ");
   for (i = 0; i < SIZE; i++){
-    nums[i] = scanf("%lf", &input);
+    scanf("%lf", &input);
+    nums[i] = input;
   }
-
-  for (i = 0; i < SIZE; i++){
-    printf("%lf ", nums[i]);
+  double sum = 0;
+  for (j = 0; j < SIZE; j++){
+    map_nums[j] = nums[j] + sum;
+    sum = map_nums[j];
+    printf("%.6lf", map_nums[j]);
+    printf(" -> ");
   }
-
   return 0;
 }
 
